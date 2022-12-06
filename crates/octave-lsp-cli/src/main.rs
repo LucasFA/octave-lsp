f87c0966b56c2722bbcb91c9eb9068cc607a6721
LucasFA <lucaslanzarote@gmail.com>
2022-12-06 22:06:35 +0100

    Create CLI

---
+++ crates/octave-lsp-cli/src/main.rs
@@ -0,0 +1,21 @@
+use octave_lsp::parser::Parser;
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    let mut input = String::new();
+
+    loop {
+        write!(stdout, "→ ")?;
+        stdout.flush()?;
+
+        stdin.read_line(&mut input)?;
+
+        let parse = Parser::new(&input).parse();
+        println!("{}", parse.debug_tree());
+
+        input.clear();
+    }
+}
