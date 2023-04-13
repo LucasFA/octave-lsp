LucasFA/octave-lsp

commit 1e58cfe1baddcb03c3fa2da4d802a586d7489ce2

Add Left division

crates/syntax/src/lib.rs
@@ -64,6 +64,8 @@ pub enum SyntaxKind {
     ElmtMult,
     Slash,
     ElmtDiv,
+    LeftDiv,
+    ElmtLeftDiv,
     Caret,
     ElmtPow,
     Transpose,
@@ -155,6 +157,8 @@ impl From<TokenKind> for SyntaxKind {
             TokenKind::ElmtMult => Self::ElmtMult,
             TokenKind::Slash => Self::Slash,
             TokenKind::ElmtDiv => Self::ElmtDiv,
+            TokenKind::LeftDiv => Self::LeftDiv,
+            TokenKind::ElmtLeftDiv => Self::ElmtLeftDiv,
             TokenKind::Caret => Self::Caret,
             TokenKind::ElmtPow => Self::ElmtPow,
             TokenKind::Transpose => Self::Transpose,

crates/lexer/src/token_kind.rs
@@ -91,6 +91,10 @@ pub enum TokenKind {
     Slash,
     #[token("./")]
     ElmtDiv,
+    #[token(r"\")]
+    LeftDiv,
+    #[token(r".\")]
+    ElmtLeftDiv,
     #[token("^")]
     Caret,
     #[token(".^")]

commit 990af84695d6d7c98e757da5a94def1b8df4d6c8

Fix non-keyword list
As indicated by the full coverage test, forgot to include it.
This fixes what broke in 1e58cfe1

crates/syntax/src/lib.rs
@@ -194,7 +194,7 @@ mod test {
     use crate::SyntaxKind::*;
 
     // Test with full coverage. An error here would be hard to debug
-    fn to_kw_or_not_to_kw() -> ([SyntaxKind; 25], [SyntaxKind; 41]) {
+    fn to_kw_or_not_to_kw() -> ([SyntaxKind; 25], [SyntaxKind; 43]) {
         let keywords = [
             FnKw,
             EndFnKw,
@@ -235,6 +235,8 @@ mod test {
             ElmtMult,
             Slash,
             ElmtDiv,
+            LeftDiv,
+            ElmtLeftDiv,
             Caret,
             ElmtPow,
             Transpose,