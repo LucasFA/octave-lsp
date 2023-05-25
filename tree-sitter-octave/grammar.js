db69e0fbc0f7ec9b6e890ef8fc5e9335eaffaf49
LucasFA <23667494+LucasFA@users.noreply.github.com>
2023-05-25 12:51:43 +0200

    Some writing

--- tree-sitter-octave/grammar.js
+++ tree-sitter-octave/grammar.js
@@ -1,7 +1,91 @@
+// Although languages have very different constructs, their constructs can often be categorized in to similar groups like Declarations, 
+// Definitions, Statements, Expressions, Types, and Patterns.In writing your grammar, a good first step is to create just enough 
+// structure to include all of these basic groups of symbols.For a language like Go, you might start with something like this:
+
 module.exports = grammar({
     name: 'octave',
 
     rules: {
-        source_file: $ => 'hello'
+        source_file: $ => repeat($._definition),
+
+        _definition: $ => choice(
+            $.function_definition,
+            $.variable_definition
+            // TODO: other kinds of definitions
+        ),
+
+        variable_definition: $ => seq(
+            $.identifier,
+            '=',
+            $._expression,
+            optional(';')
+        ),
+
+        function_definition: $ => seq(
+            'function',
+            $.identifier,
+            optional($.parameter_list),
+            repeat($._statement),
+            choice('endfunction', 'end')
+        ),
+
+        parameter_list: $ => seq(
+            '(',
+            optional($.parameters),
+            ')'
+        ),
+
+        parameters: $ => seq(
+            $.identifier,
+            repeat(seq(',', $.identifier))
+        )
+
+        _type: $ => choice(
+            'bool'
+            // TODO: other kinds of types
+        ),
+
+        block: $ => seq(
+            '{',
+            repeat($._statement),
+            '}'
+        ),
+
+        _statement: $ => choice(
+            $.if_statement,
+            $.while_statement,
+            $.expression_statement,
+        ),
+
+        // return_statement: $ => seq(
+        //     'return',
+        //     $._expression,
+        //     ';'
+        // ),
+
+        if_statement: $ => seq(
+            'if',
+            $._expression,
+            $.block,
+            optional($.else_clause),
+            choice('end', 'endif')
+        ),
+
+        else_clause: $ => seq(
+            'else',
+            choice(
+                $.if_statement,
+                $._expression
+            )
+        ),
+
+        _expression: $ => choice(
+            $.identifier,
+            $.number
+        ),
+
+        identifier: $ => /[a-z]+/,
+
+        number: $ => /\d+/
     }
 });
