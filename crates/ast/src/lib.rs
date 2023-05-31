f708d1cd9d75550b26c0701343e279f14e797a5d
LucasFA <23667494+LucasFA@users.noreply.github.com>
2023-05-31 21:12:12 +0200

    Fix test lower_variable_def_without_value
    Way of parsing was fundamentally broken. It still is but it passes more tests

--- crates/ast/src/lib.rs
+++ crates/ast/src/lib.rs
@@ -83,7 +83,11 @@ impl VariableDef {
     }
 
     pub fn value(&self) -> Option<Expr> {
-        self.0.children().last().and_then(Expr::cast)
+        self.0
+            .children_with_tokens()
+            .filter_map(SyntaxElement::into_node)
+            .filter_map(Expr::cast)
+            .nth(1)
     }
 }
 
